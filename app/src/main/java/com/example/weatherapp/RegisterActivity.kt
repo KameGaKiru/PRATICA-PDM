6148f78a07984df0b8ab5291d78ddf1847704c3c
Wadje <toddnunes0@gmail.com>
2025-06-02 15:46:35 -0300

    Prática 01 – Parte 4

--- app/src/main/java/com/example/weatherapp/RegisterActivity.kt
+++ app/src/main/java/com/example/weatherapp/RegisterActivity.kt
@@ -9,7 +9,14 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
@@ -22,22 +29,20 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.weatherapp.ui.theme.WeatherAppTheme
 
-class LoginActivity : ComponentActivity() {
+class RegisterActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             WeatherAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-
-                        LoginPage (modifier = Modifier.padding(innerPadding))
-
+                        RegisterPage(modifier = Modifier.padding(innerPadding)
+                    )
                 }
             }
         }
@@ -47,20 +52,33 @@ class LoginActivity : ComponentActivity() {
 @SuppressLint("ContextCastToActivity")
 @Preview(showBackground = true)
 @Composable
-fun LoginPage(modifier: Modifier = Modifier) {
+fun RegisterPage( modifier: Modifier = Modifier) {
     var email by rememberSaveable { mutableStateOf("") }
+    var userName by rememberSaveable { mutableStateOf("") }
     var password by rememberSaveable { mutableStateOf("") }
+    var passwordConfirm by rememberSaveable { mutableStateOf("") }
     val activity = LocalContext.current as? Activity
+
     Column(
-        modifier = modifier.padding(16.dp).fillMaxSize(),
+        modifier = modifier
+            .padding(16.dp)
+            .fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = CenterHorizontally,
     ) {
         Text(
-            text = "Tela inicial!",
+            text = "Tela de Registro!",
             fontSize = 24.sp
         )
-        Spacer(modifier = modifier.size(24.dp)) // espaço entre título e campo
+
+Spacer(modifier = modifier.size(12.dp)) // espaço entre título e campo
+
+        OutlinedTextField(
+            value = userName,
+            label = { Text(text = "Digite seu Nome") },
+            modifier = modifier.fillMaxWidth(fraction = 0.9f),
+            onValueChange = { userName = it }
+        )
 
         OutlinedTextField(
             value = email,
@@ -68,50 +86,42 @@ fun LoginPage(modifier: Modifier = Modifier) {
             modifier = modifier.fillMaxWidth(fraction = 0.9f),
             onValueChange = { email = it }
         )
-        Spacer(modifier = modifier.size(24.dp)) //espaço entre campos
 
         OutlinedTextField(
             value = password,
-            label = { Text(text = "Digite sua senha") },
+            label = { Text(text = "Digite sua senha!") },
             modifier = modifier.fillMaxWidth(fraction = 0.9f),
-            onValueChange = { password = it },
-            visualTransformation = PasswordVisualTransformation()
+            onValueChange = { password = it }
         )
-        Spacer(modifier = modifier.size(24.dp)) // espaço antes do botão
+
+        OutlinedTextField(
+            value = passwordConfirm,
+            label = { Text(text = "Confirme sua senha!") },
+            modifier = modifier.fillMaxWidth(fraction = 0.9f),
+            onValueChange = { passwordConfirm = it }
+        )
+
+        Spacer(modifier = modifier.size(12.dp)) // espaço entre o campo e os botões
 
         Row(modifier = modifier) {
             Button(
                 onClick = {
-                    Toast.makeText(activity, "Login OK!", Toast.LENGTH_LONG).show()
-                    activity?.startActivity(
-                        Intent(activity, MainActivity::class.java).setFlags(
-                            FLAG_ACTIVITY_SINGLE_TOP
-                        )
-                    )
+                    Toast.makeText(activity, "Resgistro OK!", Toast.LENGTH_LONG).show()
+                    activity?.finish()
                 },
-        enabled = email.isNotEmpty() && password.isNotEmpty()
+                enabled = userName.isNotEmpty() && email.isNotEmpty() && password.isNotEmpty() && passwordConfirm.isNotEmpty()
 
-            ) {
-                Text("Login")
+            )
+            {
+                Text("Registrar")
             }
 
-            Button(
-                onClick = { Toast.makeText(activity, "Abrindo Registro de Usuário!", Toast.LENGTH_LONG).show()
-                    activity?.startActivity(
-                        Intent(activity, RegisterActivity::class.java).setFlags(
-                            FLAG_ACTIVITY_SINGLE_TOP
-                        )
-                    )
-        }
-            ) {
-                Text("Registro")
-            }
-
-            Button(
-                onClick = { email = ""; password = "" }
-            ) {
-                Text("Limpar")
-            }
+        Button(
+            onClick = { userName = ""; email = ""; password = ""; passwordConfirm = "" }
+        ) {
+            Text("Limpar")
         }
     }
 }
+}
+
