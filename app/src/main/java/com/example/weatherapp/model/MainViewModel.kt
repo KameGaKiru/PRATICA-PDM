485a8b2e5e1762a7471d518ba7d0e88727242794
Wadje <toddnunes0@gmail.com>
2025-06-30 14:36:56 -0300

    Prática 06 – Parte 02 - Criando Classes do Banco de Dados

--- app/src/main/java/com/example/weatherapp/model/MainViewModel.kt
+++ app/src/main/java/com/example/weatherapp/model/MainViewModel.kt
@@ -1,9 +1,16 @@
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.toMutableStateList
 import androidx.lifecycle.ViewModel
 import com.example.weatherapp.model.City
+import com.example.weatherapp.model.User
 import com.google.android.gms.maps.model.LatLng
 
 class MainViewModel : ViewModel() {
+
+    private val _user = mutableStateOf<User?> (null)
+    val user : User?
+        get() = _user.value
+
     private val _cities = getCities().toMutableStateList()
     val cities
         get() = _cities.toList()
@@ -11,9 +18,9 @@ class MainViewModel : ViewModel() {
         _cities.remove(city)
     }
     fun add(name: String, location: LatLng? = null) {
-            _cities.add(City(name = name, location = location))
-        }
+        _cities.add(City(name = name, location = location))
     }
+}
 
 private fun getCities() = List(20) { i ->
     City(name = "Cidade $i", weather = "Carregando clima...")
